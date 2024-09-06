133c04a505d846013c46648ab9461d7eb0bee309
murilopbc <murilopoltronieri3@gmail.com>
2024-09-05 21:18:53 -0300

    exercicios

--- ex5.c
+++ ex5.c
@@ -2,35 +2,21 @@
 
 int main()
 {
-    
-    float salario, soma;
+    float salario, inss;
 
     printf("Digite seu salario: ");
     scanf("%f", &salario);
 
-
-
-    if ( salario >= 4000,03){
-
-        salario * 0.14;
-        printf("\nValor de contribuição do Inss: %.2f ", salario);
-    }
     
-    if ( salario >= 2666,68){
-
-        salario * 0.12;
-        printf("\nValor de contribuição do Inss: %.2f ", salario);
-    }
-    
-    if ( salario >= 4000,03){
-
-        salario * 0.09;
-        printf("\nValor de contribuição do Inss: %.2f ", salario);
-    }
-    else {
-        salario * 0.09;
-        printf("\nValor de contribuição do Inss: %.2f ", salario);
+    if (salario >= 4000.03 && salario <= 7786.02) {
+        inss = (salario * 0.14) - 181.18;
+    } else if (salario >= 2666.68) {
+        inss = (salario * 0.12) - 101.18;
+    } else if (salario >= 1412) {
+        inss = (salario * 0.09) - 21.18;
+    } else {
+        inss = salario * 0.075;
     }
 
-    
+    printf("\nValor de contribuicao do INSS: %.2f\n", inss);
 }
