89da2f104eb80e3c4188d5461114aea1957f4fc5
murilopbc <murilopoltronieri3@gmail.com>
2024-09-05 21:31:46 -0300

    ex5

--- ex5.c
+++ ex5.c
@@ -9,11 +9,11 @@ int main()
 
     
     if (salario >= 4000.03 && salario <= 7786.02) {
-        inss = (salario * 0.14) - 181.18;
+        inss = (salario * 0.14) + 181.18;
     } else if (salario >= 2666.68) {
-        inss = (salario * 0.12) - 101.18;
+        inss = (salario * 0.12) + 101.18;
     } else if (salario >= 1412) {
-        inss = (salario * 0.09) - 21.18;
+        inss = (salario * 0.09) + 21.18;
     } else {
         inss = salario * 0.075;
     }
